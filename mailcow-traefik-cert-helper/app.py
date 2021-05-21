3144f99753a3bbe6c2f14c20af4ea5dd5301a92d
Alexander Worlitschek <github@olqs.de>
2021-05-21 17:01:13 +0200

    initial commit of mailcow-traefik-cert-helper

---
+++ mailcow-traefik-cert-helper/app.py
@@ -0,0 +1,42 @@
+from flask import Flask,Response
+from flaskext.mysql import MySQL
+import yaml
+import os
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = os.environ.get("MYSQL_DATABASE_USER", default='mailcow')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get("MYSQL_DATABASE_PASSWORD", default='mailcow')
+app.config['MYSQL_DATABASE_PORT'] = os.environ.get("MYSQL_DATABASE_PORT", default=3306)
+app.config['MYSQL_DATABASE_DB'] = os.environ.get("MYSQL_DATABASE_DB", default='mailcow')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get("MYSQL_DATABASE_HOST", default='mariadb')
+
+TRAEFIK_ROUTER = os.environ.get("TRAEFIK_ROUTER", default="nginx-mailcow-secure")
+
+mysql.init_app(app)
+
+@app.route('/')
+def hello():
+  try:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT domain from domain;")
+    rows=cursor.fetchall()
+    tls_domains = []
+
+    for row in rows:
+      subdomains = ["main", "imap", "smtp", "pop3", "autodiscover", "autoconfig", "webmail", "email"]
+      domains = [ domain + "." + row[0] for domain in subdomains]
+      tls_domains += [ { 'main' : "mail." + row[0], 'sans' : domains }]
+    
+    traefik_config = {'traefik' : { 'http' : { 'routers' : { TRAEFIK_ROUTER : { 'tls' : { 'domains' : tls_domains } }}}}}
+    response = yaml.dump(traefik_config)
+        
+    return Response(response, mimetype='text/plain')
+  except Exception as e:
+    print(e)
+  finally:
+    cursor.close()
+    conn.close()
+
