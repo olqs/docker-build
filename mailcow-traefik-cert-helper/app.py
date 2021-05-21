55f01fd835e0b928c67274e47bcec9b6d1980f24
Alexander Worlitschek <github@olqs.de>
2021-05-21 18:07:37 +0200

    build complete traefik router config

--- mailcow-traefik-cert-helper/app.py
+++ mailcow-traefik-cert-helper/app.py
@@ -10,7 +10,7 @@ app.config['MYSQL_DATABASE_USER'] = os.environ.get("MYSQL_DATABASE_USER", defaul
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get("MYSQL_DATABASE_PASSWORD", default='mailcow')
 app.config['MYSQL_DATABASE_PORT'] = os.environ.get("MYSQL_DATABASE_PORT", default=3306)
 app.config['MYSQL_DATABASE_DB'] = os.environ.get("MYSQL_DATABASE_DB", default='mailcow')
-app.config['MYSQL_DATABASE_HOST'] = os.environ.get("MYSQL_DATABASE_HOST", default='mariadb')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get("MYSQL_DATABASE_HOST", default='localhost')
 
 TRAEFIK_ROUTER = os.environ.get("TRAEFIK_ROUTER", default="nginx-mailcow-secure")
 
@@ -26,11 +26,26 @@ def hello():
     tls_domains = []
 
     for row in rows:
-      subdomains = ["main", "imap", "smtp", "pop3", "autodiscover", "autoconfig", "webmail", "email"]
+      subdomains = ["main", "imap", "smtp", "pop3", "autodiscover", "autoconfig", "webmail", "email", "mailtest"]
       domains = [ domain + "." + row[0] for domain in subdomains]
       tls_domains += [ { 'main' : "mail." + row[0], 'sans' : domains }]
-    
-    traefik_config = {'traefik' : { 'http' : { 'routers' : { TRAEFIK_ROUTER : { 'tls' : { 'domains' : tls_domains } }}}}}
+    traefik_config = {'traefik' : 
+      { 'http' : 
+        { 'routers' : 
+          { TRAEFIK_ROUTER : 
+            { 'tls' : 
+              { 
+                'domains' : tls_domains,
+                'certresolver' : 'http'
+              },
+              'endpoints' : 'https',
+              'rule' : 'Host(`mailtest.worli.info`)',
+              'service' : 'nginx-mailcow',
+            }
+          }
+        }
+      }
+    }
     response = yaml.dump(traefik_config)
         
     return Response(response, mimetype='text/plain')
